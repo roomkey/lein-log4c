b2e244c6a2a9a89c8f93981785d980e0a8a0316f
Chris Hapgood <cch1@hapgoods.com>
2012-05-06 18:20:37 -0400

    Relax injected depdency on log4c

--- src/leiningen/log4c.clj
+++ src/leiningen/log4c.clj
@@ -5,7 +5,7 @@
 (defn log4c [eip project form & [handler skip-auto-compile init]]
   (let [project (if (some #(= 'com.hotelicopter/log4c (first %)) (:dev-dependencies project))
                   project
-                  (update-in project [:dev-dependencies] conj ['com.hotelicopter/log4c "1.2.0"]))
+                  (update-in project [:dev-dependencies] conj ['com.hotelicopter/log4c "[1.2.0,2.0)"]))
         form `(do (clj-logging-config.log4j/set-logger!)
                   (log4c.core/configure-log-levels!)
                   ~form)
