521af1266f106ac590916007d882c8b40b3c64a5
Chris Hapgood <cch1@hapgoods.com>
2012-05-06 11:48:35 -0400

    Initial commit of project skeleton

---
+++ project.clj
@@ -0,0 +1,5 @@
+(require '[leiningen.v :as version])
+
+(defproject com.hotelicopter/lein-log4c (version/abbrev)
+  :description "A Leiningen plugin to configure the log4j configuration"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
