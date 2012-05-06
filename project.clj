ff5d6ee57efc38bc12e467402971233c998a9f07
Chris Hapgood <cch1@hapgoods.com>
2012-05-06 17:59:23 -0400

    Expose compatibility with multiple clojure versions

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
 
 (defproject com.hotelicopter/lein-log4c (version/abbrev)
   :description "A Leiningen plugin to configure the log4j configuration"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "[1.2.1,1.3.0]"]])
