09ab9c441504cf167e4471b0213fb606c6920825
Chris Hapgood <cch1@hapgoods.com>
2012-05-07 20:49:28 -0400

    Use latest lein-version

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(require '[leiningen.v :as version])
-
-(defproject com.hotelicopter/lein-log4c (version/abbrev)
+(defproject com.hotelicopter/lein-log4c "lein-version"
   :description "A Leiningen plugin to configure the log4j configuration"
-  :dependencies [[org.clojure/clojure "[1.2.1,1.3.0]"]])
+  :dependencies [[org.clojure/clojure "[1.2.1,1.3.0]"]]
+  :hooks [leiningen.v])
