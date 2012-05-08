d4e28ea7807a5b143486ebfaff2fe74108e9725e
Chris Hapgood <cch1@hapgoods.com>
2012-05-08 15:00:32 -0400

    Use newest log4c to handle initialization

--- src/leiningen/log4c.clj
+++ src/leiningen/log4c.clj
@@ -2,7 +2,7 @@
   (:require [leiningen.compile :only [eval-in-project]]
             [robert.hooke :as hook]))
 
-(let [addl-deps {'com.hotelicopter/log4c "[1.2.0,2.0)"
+(let [addl-deps {'com.hotelicopter/log4c "[1.2.1,2.0)"
                  'log4j/log4j "[1.2.16,2.0)"}]
   (defn log4c [eip project form & [handler skip-auto-compile init]]
     (let [names (set (map first (:dev-dependencies project)))
@@ -11,13 +11,12 @@
           project (assoc project :dev-dependencies deps)
           form `(do (alter-var-root (var clojure.tools.logging/*logger-factory*)
                                     (constantly (clojure.tools.logging.impl/log4j-factory)))
-                    (clj-logging-config.log4j/set-logger!)
+                    (log4c.core/init!)
                     (log4c.core/configure-log-levels!)
                     ~form)
           init `(do (require 'clojure.tools.logging)
                     (require 'clojure.tools.logging.impl)
                     (require 'log4c.core)
-                    (require 'clj-logging-config.log4j)
                     ~init)]
       (eip project form handler skip-auto-compile init))))
 
