b639fe32bfad1ee46ef5d7c3efa091b0e8aac192
Chris Hapgood <cch1@hapgoods.com>
2012-05-08 15:30:39 -0400

    Add hotelicopter repositories to project

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject com.hotelicopter/lein-log4c "lein-version"
   :description "A Leiningen plugin to configure the log4j configuration"
   :dependencies [[org.clojure/clojure "[1.2.1,1.3.0]"]]
+  :repositories {"releases" ~(str user/local-maven-clone "/releases")
+                 "snapshots" ~(str user/local-maven-clone "/snapshots")
+                 "hotelicopter_snapshots" "https://raw.github.com/g1nn13/maven/master/snapshots"
+                 "hotelicopter_releases" "https://raw.github.com/g1nn13/maven/master/releases"}
   :hooks [leiningen.v])
