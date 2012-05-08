840f34e4ac3fc2b5a937f103b094397e0661325d
Chris Hapgood <cch1@hapgoods.com>
2012-05-07 21:31:31 -0400

    Bind log4j to clojure.tools.logging/*logger-factory*
     * Inject additional dependencies as needed.

--- src/leiningen/log4c.clj
+++ src/leiningen/log4c.clj
@@ -2,17 +2,24 @@
   (:require [leiningen.compile :only [eval-in-project]]
             [robert.hooke :as hook]))
 
-(defn log4c [eip project form & [handler skip-auto-compile init]]
-  (let [project (if (some #(= 'com.hotelicopter/log4c (first %)) (:dev-dependencies project))
-                  project
-                  (update-in project [:dev-dependencies] conj ['com.hotelicopter/log4c "[1.2.0,2.0)"]))
-        form `(do (clj-logging-config.log4j/set-logger!)
-                  (log4c.core/configure-log-levels!)
-                  ~form)
-        init `(do (require 'log4c.core)
-                  (require 'clj-logging-config.log4j)
-                  ~init)]
-    (eip project form handler skip-auto-compile init)))
+(let [addl-deps {'com.hotelicopter/log4c "[1.2.0,2.0)"
+                 'log4j/log4j "[1.2.16,2.0)"}]
+  (defn log4c [eip project form & [handler skip-auto-compile init]]
+    (let [names (set (map first (:dev-dependencies project)))
+          ;; Merge dependencies, preserving order and omitting duplicate names
+          deps (reduce (fn [m me] (if (names (key me)) m (conj m me))) (:dev-dependencies project) addl-deps)
+          project (assoc project :dev-dependencies deps)
+          form `(do (alter-var-root (var clojure.tools.logging/*logger-factory*)
+                                    (constantly (clojure.tools.logging.impl/log4j-factory)))
+                    (clj-logging-config.log4j/set-logger!)
+                    (log4c.core/configure-log-levels!)
+                    ~form)
+          init `(do (require 'clojure.tools.logging)
+                    (require 'clojure.tools.logging.impl)
+                    (require 'log4c.core)
+                    (require 'clj-logging-config.log4j)
+                    ~init)]
+      (eip project form handler skip-auto-compile init))))
 
 (defn activate []
   (hook/add-hook #'leiningen.compile/eval-in-project log4c))
