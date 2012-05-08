effc82e1375e207b3d6645caeaccb53179606a39
Chris Hapgood <cch1@hapgoods.com>
2012-05-08 16:41:52 -0400

    Gracefully handle lack of logging API

--- src/leiningen/log4c.clj
+++ src/leiningen/log4c.clj
@@ -9,14 +9,16 @@
           ;; Merge dependencies, preserving order and omitting duplicate names
           deps (reduce (fn [m me] (if (names (key me)) m (conj m me))) (:dev-dependencies project) addl-deps)
           project (assoc project :dev-dependencies deps)
-          form `(do (alter-var-root (var clojure.tools.logging/*logger-factory*)
-                                    (constantly (clojure.tools.logging.impl/log4j-factory)))
-                    (log4c.core/init!)
-                    (log4c.core/configure-log-levels!)
+          form `(do (try (alter-var-root (var clojure.tools.logging/*logger-factory*)
+                                         (constantly (clojure.tools.logging.impl/log4j-factory)))
+                         (log4c.core/init!)
+                         (log4c.core/configure-log-levels!)
+                         (catch Throwable _#))
                     ~form)
-          init `(do (require 'clojure.tools.logging)
-                    (require 'clojure.tools.logging.impl)
-                    (require 'log4c.core)
+          init `(do (try (require 'clojure.tools.logging)
+                         (require 'clojure.tools.logging.impl)
+                         (require 'log4c.core)
+                         (catch Throwable _#))
                     ~init)]
       (eip project form handler skip-auto-compile init))))
 
