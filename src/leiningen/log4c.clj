2dc0115bcbe6b2b47d687842c1b135aaa62a5db1
Chris Hapgood <cch1@hapgoods.com>
2012-05-14 16:36:01 -0400

    Scale back ambitions to control implementation choice
    Focus on configuring log4j/jul via clj-logging-config.
    
     * Let log4c specify its own dependencies (currently log4j).
     * Let standard dependency management techniques determine the actual
       runtime logging implementation behind clojure.tools.logging.
       (for effective use of this tool, log4j must be used, directly or
       via slf4j).

--- src/leiningen/log4c.clj
+++ src/leiningen/log4c.clj
@@ -2,23 +2,18 @@
   (:require [leiningen.compile :only [eval-in-project]]
             [robert.hooke :as hook]))
 
-(let [addl-deps {'com.hotelicopter/log4c "[1.2.1,2.0)"
-                 'log4j/log4j "[1.2.16,2.0)"}
+(let [addl-deps {'com.hotelicopter/log4c "[1.2.1,2.0)"}
       config (get (System/getenv) "CLJ_LOG_LEVEL")]
   (defn log4c [eip project form & [handler skip-auto-compile init]]
     (let [names (set (map first (:dev-dependencies project)))
           ;; Merge dependencies, preserving order and omitting duplicate names
           deps (reduce (fn [m me] (if (names (key me)) m (conj m me))) (:dev-dependencies project) addl-deps)
           project (assoc project :dev-dependencies deps)
-          form `(do (try (alter-var-root (var clojure.tools.logging/*logger-factory*)
-                                         (constantly (clojure.tools.logging.impl/log4j-factory)))
-                         (log4c.core/init!)
+          form `(do (try (log4c.core/init!)
                          (log4c.core/configure-log-levels! ~config)
                          (catch Throwable _#))
                     ~form)
-          init `(do (try (require 'clojure.tools.logging)
-                         (require 'clojure.tools.logging.impl)
-                         (require 'log4c.core)
+          init `(do (try (require 'log4c.core)
                          (catch Throwable _#))
                     ~init)]
       (eip project form handler skip-auto-compile init))))
