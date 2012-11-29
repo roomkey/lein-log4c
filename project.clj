68acdaf2e677e83f32766cfd54198aae25e2125b
Chris Hapgood <cch1@hapgoods.com>
2012-11-29 18:39:42 -0500

    Fix license format for more happier leiningen

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject com.hotelicopter/lein-log4c "lein-v"
   :description "A Leiningen plugin to configure the log4j configuration"
   :url "https://github.com/g1nn13/lein-log4c"
-  :license "none"
+  :license {:name "None"
+            :distribution :manual
+            :comments "All rights reserved"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :repositories {"releases" {:url "s3p://rk-maven/releases/"}}
   :plugins [[s3-wagon-private "1.1.2"]
