f6d424f423351238ce8ab9112b3ee1054cb0637b
Chris Hapgood <cch1@hapgoods.com>
2012-11-29 18:31:44 -0500

    Add missing keys in project.clj for nirvana

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject com.hotelicopter/lein-log4c "lein-v"
   :description "A Leiningen plugin to configure the log4j configuration"
+  :url "https://github.com/g1nn13/lein-log4c"
+  :license "none"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :repositories {"releases" {:url "s3p://rk-maven/releases/"}}
   :plugins [[s3-wagon-private "1.1.2"]
