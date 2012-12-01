b62a8285fa7d6f2e6c66ccc522e0157c33db336c
Chris Hapgood <cch1@hapgoods.com>
2012-11-30 23:32:44 -0500

    Don't force the issue with tasks that don't include the dev profile
    And allow init argument to be optional (which is required to safely wrap
    leiningen's own eval-in-project).

--- src/leiningen/log4c.clj
+++ src/leiningen/log4c.clj
@@ -1,11 +1,16 @@
 (ns leiningen.log4c)
 
-(defn log4c [eip project form init]
-  (let [form `(do (try (log4c.core/init!)
-                       (log4c.core/configure-log-levels!)
-                       (catch Throwable _#))
-                  ~form)
-        init `(do (try (require 'log4c.core)
-                       (catch Throwable _#))
-                  ~init)]
+(defn- log4c-included? [{dependencies :dependencies}]
+  (some (fn [[d v]] (= 'com.hotelicopter/log4c d) ) dependencies))
+
+;; TODO: remove this from list of available tasks
+(defn log4c [eip project form & [init]]
+  ;; Don't force the issue with tasks that don't include the dev profile
+  (if (log4c-included? project)
+    (let [form `(do (log4c.core/init!)
+                    (log4c.core/configure-log-levels!)
+                    ~form)
+          init `(do (require 'log4c.core)
+                    ~init)]
+      (eip project form init))
     (eip project form init)))
