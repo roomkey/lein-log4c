d351151510c7511ab40dfd0a3c055327ef057b74
Chris Hapgood <cch1@hapgoods.com>
2013-01-30 11:00:11 -0500

    Update to latest version of lein-v and tweak project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
-(defproject com.hotelicopter/lein-log4c "lein-v"
+(defproject com.hotelicopter/lein-log4c :lein-v
   :description "A Leiningen plugin to configure the log4j configuration"
   :url "https://github.com/g1nn13/lein-log4c"
   :license {:name "None"
             :distribution :manual
             :comments "All rights reserved"}
+  :plugins [[s3-wagon-private "1.1.2"]
+            [com.roomkey/lein-v "3.3.4"]]
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :repositories {"releases" {:url "s3p://rk-maven/releases/"}}
-  :plugins [[s3-wagon-private "1.1.2"]
-            [com.roomkey/lein-v "3.1.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :profiles {:dev {:resource-paths ["test-resources"]
+                   :dependencies [[midje "1.4.0"]]}}
   :eval-in-leiningen true)
