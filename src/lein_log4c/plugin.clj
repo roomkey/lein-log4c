b50b9f86258d2ce2df7e73ab4db2573ee4416628
Chris Hapgood <cch1@hapgoods.com>
2013-01-31 13:13:58 -0500

    Update to work with Leiningen release version 2.0 & scrap phantom task

--- src/lein_log4c/plugin.clj
+++ src/lein_log4c/plugin.clj
@@ -1,13 +1,25 @@
 (ns lein-log4c.plugin
   (:require [leiningen.compile :only [eval-in-project]]
-            [leiningen.log4c]
             [robert.hooke]))
 
-(defn hooks []
-  (robert.hooke/add-hook #'leiningen.core.eval/eval-in-project leiningen.log4c/log4c))
+(defn- log4c-included? [{dependencies :dependencies}]
+  (some (fn [[d v]] (= 'com.hotelicopter/log4c d) ) dependencies))
 
-(defn middleware [project]
-  (update-in project [:profiles :dev :dependencies]
-             (fn [deps]
-               ;; It appears that leiningen handles duplicates reasonably
-               (conj (or deps []) ['com.hotelicopter/log4c "1.2.2"]))))
+(defn- configure-logging [eip project form & [init]]
+  (let [form `(do (log4c.core/init!)
+                  (log4c.core/configure-log-levels!)
+                  ~form)
+        init `(do (require 'log4c.core)
+                  ~init)]
+    (eip project form init)))
+
+(defn- inject-dependency [eip project form & [init]]
+  (let [project (update-in project [:dependencies] conj ['com.hotelicopter/log4c "1.2.2"])]
+    (eip project form init)))
+
+;; We inject the dependency in an eip hook because adding it in
+;; middleware is too intrusive (it must be added to the project
+;; itself as profiles have already been merged into the project).
+(defn hooks []
+  (robert.hooke/add-hook #'leiningen.core.eval/eval-in-project inject-dependency)
+  (robert.hooke/add-hook #'leiningen.core.eval/eval-in-project configure-logging))
